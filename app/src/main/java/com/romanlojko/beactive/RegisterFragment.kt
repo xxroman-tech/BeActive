131aa52ec4f11ffc3581182d400003565ebbc867
Roman Lojko <roman.lojko@brainit.sk>
2022-04-20 11:34:53 +0200

    #8 Pridanie Register fragmentu

---
+++ app/src/main/java/com/romanlojko/beactive/RegisterFragment.kt
@@ -0,0 +1,36 @@
+package com.romanlojko.beactive
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.romanlojko.beactive.databinding.FragmentRegisterBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegisterFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegisterFragment : Fragment() {
+
+    lateinit var binding: FragmentRegisterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(layoutInflater)
+
+        binding.registerButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_registerFragment_to_mainApplication)
+        }
+    }
+
+}
